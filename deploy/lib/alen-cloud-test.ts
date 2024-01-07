3b63a672675b5d8f435d5f8fbdc85254596a7d39
bulingen <adamlecorney@gmail.com>
2024-01-07 11:52:02 +0100

    feat: install cyclonedds on instance

--- deploy/lib/alen-cloud-test.ts
+++ deploy/lib/alen-cloud-test.ts
@@ -75,7 +75,7 @@ export class AlenCloudTest extends cdk.Stack {
       'echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null',
       "sudo apt update",
       "sudo apt upgrade -y",
-      "sudo apt install ros-humble-desktop -y"
+      "sudo apt install ros-humble-desktop ros-humble-rmw-cyclonedds-cpp -y"
     );
 
     userData.addCommands("Done with user data stuffs!");
