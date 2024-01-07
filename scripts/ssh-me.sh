b8a8b60a5fc126b2c09f3aef149f966936d5a120
bulingen <adamlecorney@gmail.com>
2024-01-07 10:50:41 +0100

    fix: make ssh-me script work

--- scripts/ssh-me.sh
+++ scripts/ssh-me.sh
@@ -3,7 +3,14 @@ DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "$DIR" ]]; then DIR="$PWD"; fi
 . "$DIR/common.sh"
 
+sshUsingOwnKey() {
+    user=$1
+    dns=$2
+    ssh $user@$dns
+}
+
 echo "Getting public DNS for server with name $serverName"
 publicDns=$(getPublicDns "$serverName")
-echo "SSH to $publicDns using your public key"
-ssh $userName@$publicDns
+echo SSH to $publicDns using your public key
+
+sshUsingOwnKey $userName $publicDns
