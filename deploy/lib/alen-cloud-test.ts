b0250921608b4e46513e1e57b48af1b8e0d68fdd
bulingen <adamlecorney@gmail.com>
2024-01-07 11:50:55 +0100

    feat: install ros2 on instance

--- deploy/lib/alen-cloud-test.ts
+++ deploy/lib/alen-cloud-test.ts
@@ -66,6 +66,20 @@ export class AlenCloudTest extends cdk.Stack {
       `npm install --global yarn`
     );
 
+    userData.addCommands(
+      'echo "Installing ROS2"',
+      "sudo apt install software-properties-common -y",
+      "sudo add-apt-repository universe",
+      "sudo apt update && sudo apt install curl -y",
+      "sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg",
+      'echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null',
+      "sudo apt update",
+      "sudo apt upgrade -y",
+      "sudo apt install ros-humble-desktop -y"
+    );
+
+    userData.addCommands("Done with user data stuffs!");
+
     const instance = new cdk.aws_ec2.Instance(this, instanceName, {
       vpc,
       role: role,
